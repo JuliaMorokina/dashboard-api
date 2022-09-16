2f45f6c164735a632830388c6a3843b712d6ec71
Julia Morokina <juliamorokina@mail.ru>
2022-09-16 17:08:43 +0300

    Промежуточные обработчики

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { userRouter } from "./users/users.js";
 const port = 8000;
 const app = express();
 
@@ -9,13 +9,20 @@ app.all("/hello", (req, res, next) => {
 });
 
 app
-  .route("/user")
   .get("/hello", (req, res) => {
-    res.send("Hello! I'm simple server! GET");
+    // res.send({ success: true });
+    throw new Error("");
   })
   .post("/hello", (req, res) => {
-    res.send("Hello! I'm simple server! POST");
+    res.set("Content-Type", "application/json");
+    res.status(201).json({ success: true });
   });
+
+app.use("/users", userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+});
 
 app.listen(port, () => {
   console.log("server listeting");
