a47237481add027091d3095e94bfefd7fa84faf3
Julia Morokina <juliamorokina@mail.ru>
2022-09-20 23:13:33 +0300

    Базовый класс контроллера

---
+++ src/common/route.interface.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response, Router } from "express";
+
+export interface IControllerRoute {
+  func: (req: Request, res: Response, next: NextFunction) => void;
+  method: keyof Pick<Router, "get" | "post" | "delete" | "put" | "patch">;
+  path: string;
+}
