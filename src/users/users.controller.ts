170341898c35f29db01b34d5d7df03c16881dbcb
Julia Morokina <juliamorokina@mail.ru>
2022-09-21 23:13:24 +0300

    users controller [new][S] - there was created user controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { BaseCotroller } from "../common/base.controller";
+import { IControllerRoute } from "../common/route.interface";
+import { LoggerService } from "../logger/logger.service";
+
+export class UsersController extends BaseCotroller {
+  private routes: IControllerRoute[] = [
+    {
+      func: this.login,
+      method: "post",
+      path: "/login"
+    },
+    {
+      func: this.registration,
+      method: "post",
+      path: "/registration"
+    }
+  ];
+
+  constructor(logger: LoggerService) {
+    super(logger);
+    this.bindRoutes(this.routes);
+  }
+
+  login(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, "Success");
+  }
+
+  registration(req: Request, res: Response, next: NextFunction) {
+    this.send(res, 201, "Success registration");
+  }
+}
