c717f1f60a6df16524ab591d2c0bbbe0e36ceeba
Julia Morokina <juliamorokina@mail.ru>
2022-09-25 19:59:29 +0300

    Data transfer object for users

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import { json } from 'body-parser';
 import { inject, injectable } from 'inversify';
 import { Server } from 'http';
 import { ExeptionFilter } from './errors/exeption.filter';
@@ -22,6 +23,10 @@ export class App {
 		this.port = 8000;
 	}
 
+	useMiddleware(): void {
+		this.app.use(json());
+	}
+
 	useRoutes(): void {
 		this.app.use('/users', this.usersController.router);
 	}
@@ -31,6 +36,7 @@ export class App {
 	}
 
 	public async init(): Promise<void> {
+		this.useMiddleware();
 		this.useRoutes();
 		this.useExeptionsFilters();
 		this.server = this.app.listen(this.port);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,8 @@ import { BaseCotroller } from '../common/base.controller';
 import { IControllerRoute } from '../common/route.interface';
 import { IUserController } from './users.controller.interface';
 import { ILogger } from '../logger/logger.interface';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegistrationDto } from './dto/user-registration.dto';
 import { TYPES } from '../types';
 import 'reflect-metadata';
 
@@ -27,11 +29,13 @@ export class UsersController extends BaseCotroller implements IUserController {
 		this.bindRoutes(this.routes);
 	}
 
-	login(req: Request, res: Response, next: NextFunction): void {
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
 		this.ok(res, 'Success');
 	}
 
-	registration(req: Request, res: Response, next: NextFunction): void {
+	registration(req: Request<{}, {}, UserRegistrationDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
 		this.send(res, 201, 'Success registration');
 	}
 }
